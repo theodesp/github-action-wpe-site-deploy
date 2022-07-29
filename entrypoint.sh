7dde9c448adff9cb36f848cddbc999b4e63f62a9
Daniel Savo <daniel.savo@wpengine.com>
2022-07-29 15:15:21 -0500

    Add github_action_repo to output

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,7 @@ fi
 echo "Deploying ${GITHUB_REF} to ${WPE_ENV_NAME} ..." 
 echo "GITHUB_ACTION = ${GITHUB_ACTION}"
 echo "GITHUB_JOB = ${GITHUB_JOB}"
+echo "GITHUB_ACTION_REPOSITORY = ${GITHUB_ACTION_REPOSITORY}"
 
 # Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
